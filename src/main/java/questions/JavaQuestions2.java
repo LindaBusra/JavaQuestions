a58819cc6f8a0f1e632645314fb16fdc57fb4200
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-05-20 19:13:42 +0200

    last

--- src/main/java/questions/JavaQuestions2.java
+++ src/main/java/questions/JavaQuestions2.java
@@ -9,10 +9,10 @@ public class JavaQuestions2 {
 
 // 7. Write a Java program to find the index of an element in an integer array.
 
-        int arr[] = new int[]{4,8,9,12,-6,5};
+        int arr[] = new int[]{4, 8, 9, 12, -6, 5};
         int element = 14;
         int index = indexOfElementInArray(arr, element);
-        if(index == -1){
+        if (index == -1) {
             System.out.println(element + " does not exist in this array");
         } else {
             System.out.println("The index of " + element + " in the array: " + index);
@@ -21,7 +21,7 @@ public class JavaQuestions2 {
 
 // 8. Declare and initialize a boolean variable. Use an if statement to print out "True" if true, otherwise print "False".
         boolean isYoung = false;
-        if(isYoung){
+        if (isYoung) {
             System.out.println("True");
         } else {
             System.out.println("False");
@@ -29,7 +29,7 @@ public class JavaQuestions2 {
 
 
 // 9. Create a multidimensional array and access the second element of the first array.
-        int multiarr[][] = {{2,3}, {8,9}, {9,3}, {-6, 5}};
+        int multiarr[][] = {{2, 3}, {8, 9}, {9, 3}, {-6, 5}};
         System.out.println(Arrays.deepToString(multiarr)); // Printing the whole array
         int secondElementOfFirstArray = multiarr[0][1]; // Accessing the second element of the first array
         System.out.println("Second element of the first array: " + secondElementOfFirstArray);
@@ -37,7 +37,7 @@ public class JavaQuestions2 {
 
 // 10. Write a Java program to reverse a string and print it.
         String text = "Hello World";
-        for(int i=text.length()-1; i>=0; i--){
+        for (int i = text.length() - 1; i >= 0; i--) {
             System.out.print(text.charAt(i));
         }
         System.out.println("\n\n");
@@ -65,18 +65,30 @@ public class JavaQuestions2 {
         System.out.println("after remove: " + list);
 
 
-
 // 12. Write a method that checks if a string is a palindrome or not.
 
-
-
-
-
-
-
-
+        System.out.println(isPalindrome("radar"));
 
     }
+
+    public static boolean isPalindrome(String text) {
+
+        String resv = "";
+        String arr[] = text.split("");
+        for (int i = text.length() - 1; i >= 0; i--) {
+            resv += arr[i];
+        }
+
+        if(text.equals(resv)){
+            System.out.println(text + " is polindrome");
+            return true;
+        } else {
+            System.out.println(text + " is not polindrome");
+            return  false;
+        }
+
+    }
+
 
 
 

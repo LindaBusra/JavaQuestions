becc1bfe15f971658cc0cd41898a2ad39ad1cb87
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-04 22:54:08 +0200

    last

--- src/main/java/questions/JavaQuestions7.java
+++ src/main/java/questions/JavaQuestions7.java
@@ -91,13 +91,6 @@ public class JavaQuestions7 {
     }
 
 
-
-
-
-
-
-
-
     }
 
 
---
+++ src/main/java/questions/JavaQuestions8.java
@@ -0,0 +1,8 @@
+package questions;
+
+public class JavaQuestions8 {
+    public static void main(String[] args) {
+
+
+    }
+}

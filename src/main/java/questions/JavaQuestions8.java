c45dadaca87cd4d2ecf25e2d41b81161f7415ff7
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-05 19:34:46 +0200

    last

--- src/main/java/questions/JavaQuestions8.java
+++ src/main/java/questions/JavaQuestions8.java
@@ -7,8 +7,6 @@ public class JavaQuestions8 {
     public static void main(String[] args) {
 
 // 47. Create a method that takes a List of integers and returns it with all duplicates removed.
-// 48. Initialize a String array with some values and use a loop to print each element.
-// 49. Write a program using a switch statement that prints the name of the month based on the number input (1 to 12).
         List<Integer> list = new ArrayList<>();
         list.add(5);
         list.add(25);
@@ -18,6 +16,17 @@ public class JavaQuestions8 {
         list.add(51);
         list.add(53);
         System.out.println(removeDuplicateItemFromArray(list));
+
+
+// 48. Initialize a String array with some values and use a loop to print each element.
+        String arr[] = new String[]{"Ole", "Mark", "Jack", "Nils", "Jeny"};
+        for(String w: arr){
+            System.out.println(w);
+        }
+
+
+// 49. Write a program using a switch statement that prints the name of the month based on the number input (1 to 12).
+
 
 
     }

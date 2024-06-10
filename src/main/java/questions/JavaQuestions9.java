d2cde66422d6e0fdae80e3f4c56776ff24c4af83
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-10 19:53:22 +0200

    last

--- src/main/java/questions/JavaQuestions9.java
+++ src/main/java/questions/JavaQuestions9.java
@@ -1,4 +1,21 @@
 package questions;
 
+import java.text.DecimalFormat;
+
 public class JavaQuestions9 {
+
+    public static void main(String[] args) {
+
+// 52. Declare a float variable with a value and print it rounded to two decimal places.
+
+        float num = 7.8386F;
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        System.out.println(decimalFormat.format(num));
+
+// 53. Create an ArrayList of Strings. Add some colors (as string elements) and then use a loop to print each color.
+// 54. Write a program that takes a string input and prints the number of words in the string.
+// 55. Initialize an integer array. Use a for loop to print all elements that are divisible by 3.
+
+
+    }
 }
--- src/main/java/questions/JavaQuestions_all.java
+++ src/main/java/questions/JavaQuestions_all.java
@@ -68,6 +68,8 @@ public class JavaQuestions_all {
 // 53. Create an ArrayList of Strings. Add some colors (as string elements) and then use a loop to print each color.
 // 54. Write a program that takes a string input and prints the number of words in the string.
 // 55. Initialize an integer array. Use a for loop to print all elements that are divisible by 3.
+
+
 // 56. Write a method in Java that checks if a given string starts with the character 'S'.
 // 57. Create a HashMap where you store countries as keys and their capitals as values. Print all keys and values.
 // 58. Write a Java program to print a square pattern of '*' with a side of 5 units.

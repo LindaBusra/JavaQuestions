cdb545705183106395f1f06cae1c1a2c11673117
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-10 20:42:20 +0200

    last

--- src/main/java/questions/JavaQuestions9.java
+++ src/main/java/questions/JavaQuestions9.java
@@ -1,6 +1,8 @@
 package questions;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JavaQuestions9 {
 
@@ -13,6 +15,15 @@ public class JavaQuestions9 {
         System.out.println(decimalFormat.format(num));
 
 // 53. Create an ArrayList of Strings. Add some colors (as string elements) and then use a loop to print each color.
+        List<String> colors = new ArrayList<>();
+        colors.add("Blue");
+        colors.add("Purple");
+        colors.add("Black");
+        colors.add("White");
+        for(String w :colors){
+            System.out.println(w);
+        }
+
 // 54. Write a program that takes a string input and prints the number of words in the string.
 // 55. Initialize an integer array. Use a for loop to print all elements that are divisible by 3.
 

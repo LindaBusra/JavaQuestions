dac101951b9e86e66d6175c5ab76600e9e32bbae
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-18 23:22:40 +0200

    last

--- src/main/java/questions/JavaQuestion11.java
+++ src/main/java/questions/JavaQuestion11.java
@@ -10,6 +10,10 @@ public class JavaQuestion11 {
 
 // 67. Create a string variable with a value. Write a loop that prints each character of the string on a new line.
         String greeting = "Hello World";
+        String characters[] = greeting.split("");
+        for(String a : characters ){
+            System.out.println(a);
+        }
 
 // 68. Initialize a List of integers with some values. Write a method that prints the product of all elements in the List.
 // 69. Write a program using nested loops that prints a triangle pattern of '#' with a height of 5.

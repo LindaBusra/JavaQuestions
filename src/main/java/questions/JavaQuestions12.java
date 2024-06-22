62b9b79700f65abbdcd9f3bb2b1a52b3c11cb284
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-22 23:53:40 +0200

    last

--- src/main/java/questions/JavaQuestions12.java
+++ src/main/java/questions/JavaQuestions12.java
@@ -5,5 +5,9 @@ public class JavaQuestions12 {
     public static void main(String[] args) {
 
 // 73. Create a switch-case statement that converts numbers 1-5 into their respective names as strings.
+
+
+
+// 74. Write a Java program that prints the first 10 numbers of the Fibonacci series using iteration.
     }
 }

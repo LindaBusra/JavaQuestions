3a0a815298f67f752dd58598d872f257ae588195
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-25 22:55:13 +0200

    last

--- src/main/java/questions/JavaQuestions12.java
+++ src/main/java/questions/JavaQuestions12.java
@@ -6,6 +6,24 @@ public class JavaQuestions12 {
 
 // 73. Create a switch-case statement that converts numbers 1-5 into their respective names as strings.
 
+        int num = 5;
+        switch (num){
+            case 1 :
+                System.out.println("one");
+                break;
+            case 2 :
+                System.out.println("two");
+                break;
+            case 3 :
+                System.out.println("three");
+                break;
+            case 4 :
+                System.out.println("four");
+                break;
+            case 5 :
+                System.out.println("five");
+                break;
+        }
 
 
 // 74. Write a Java program that prints the first 10 numbers of the Fibonacci series using iteration.

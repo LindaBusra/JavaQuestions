925b13545935dd88ca73c657867f039406b7de80
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-06-27 22:05:56 +0200

    last

--- src/main/java/questions/JavaQuestions13.java
+++ src/main/java/questions/JavaQuestions13.java
@@ -16,7 +16,16 @@ public class JavaQuestions13 {
         } else {
             System.out.println("Invalid input!");
         }
+
+
 // 78. Declare an array of chars and use a for-each loop to print each character.
+        char []arr =  new char[]{'a', 'b', 'c', 'd'};
+        for(char w : arr){
+            System.out.println(w + " ");
+        }
+
+
+
 // 79. Create a List of doubles, add some values and then use a loop to calculate the sum of all elements.
 // 80. Write a program that checks if a string contains another string (substring).
 // 81. Initialize an integer array with random numbers and find the second smallest number.

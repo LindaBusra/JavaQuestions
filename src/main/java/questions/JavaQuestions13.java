fe0cc2a4b2f35ed7a5676ce2e014a9dd06a4bfcc
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-07-03 23:26:34 +0200

    last

--- src/main/java/questions/JavaQuestions13.java
+++ src/main/java/questions/JavaQuestions13.java
@@ -49,6 +49,7 @@ public class JavaQuestions13 {
 
 
 // 82. Write a method that takes a List of integers and prints each integer to the power of itself (n^n).
+        System.out.println(powerOfInteger(4));
 // 83. Create a Java program that calculates and prints the result of the expression (a + b * c), where a, b, and c are taken as input from the user.
 // 84. Initialize a string variable with a full sentence. Write a program that counts the number of commas in the sentence.
 
@@ -64,6 +65,18 @@ public class JavaQuestions13 {
 
 
     }
+
+    public static int powerOfInteger(int a){
+        int result = 1;
+        for(int i=1; i<=a; i++){
+            result = result*i;
+        }
+
+        return result;
+    }
+
+
+
     public static boolean isContains(String str, String text){
 
         if(str.contains(text)){

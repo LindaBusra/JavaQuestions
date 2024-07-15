6dad457db735aa1659527129280f39fcbb519bf6
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-07-15 23:53:08 +0200

    last

--- src/main/java/questions/JavaQuestions15.java
+++ src/main/java/questions/JavaQuestions15.java
@@ -12,6 +12,9 @@ public class JavaQuestions15 {
 
 
 // 92. Write a Java program to replace vowels in a string with asterisks '*'.
+        str = str.replaceAll("[aeiouAEIOU]", "*");
+        System.out.println(str);
+
 // 93. Create a method that replaces the last character of a string with the character 'z'.
 // 94. Write a Java program that asks the user for a sentence and a word to replace, and a word with which to replace it, then performs the replacement.
 // 95. Initialize a string "2024-05-07" and replace the hyphens with slashes '/'.

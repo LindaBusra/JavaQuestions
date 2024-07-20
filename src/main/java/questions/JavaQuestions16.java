7df19f28d0a44f3471c1098c08bcf3cf92c2f468
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-07-20 23:18:07 +0200

    last

--- src/main/java/questions/JavaQuestions16.java
+++ src/main/java/questions/JavaQuestions16.java
@@ -5,6 +5,11 @@ public class JavaQuestions16 {
 
 
 // 97. Create a string "Error 404: Not Found". Use replace to change "404" to "500".
+        String txt = "Error 404: Not Found";
+        txt = txt.replaceAll("404", "500");
+        System.out.println(txt);
+
+
 // 98. Write a program that replaces all occurrences of the word "error" with "warning" in a log message.
 
 // 99. Write a Java program to calculate the number of days between two dates.

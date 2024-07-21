e6ce29d0a59d7c3910b506f0a3df03832e7f0e78
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-07-21 19:08:07 +0200

    last

--- src/main/java/questions/JavaQuestions16.java
+++ src/main/java/questions/JavaQuestions16.java
@@ -11,7 +11,7 @@ public class JavaQuestions16 {
 
 
 // 98. Write a program that replaces all occurrences of the word "error" with "warning" in a log message.
-
+        replaceWord("Error is error");
 
 // 99. Write a Java program to calculate the number of days between two dates.
 // 100. Create a method that prints the name of the day (like Monday, Tuesday, etc.) for a given date.
@@ -20,7 +20,7 @@ public class JavaQuestions16 {
 
     }
     private static void replaceWord(String str){
-        str = str.replaceAll("error", "warning");
+        str = str.toLowerCase().replaceAll("error", "warning");
         System.out.println(str);
     }
 }

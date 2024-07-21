547ae74244bea08da9bb51e32de75ab95d6a473a
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-07-21 19:16:36 +0200

    last

--- src/main/java/questions/JavaQuestions16.java
+++ src/main/java/questions/JavaQuestions16.java
@@ -1,5 +1,8 @@
 package questions;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class JavaQuestions16 {
     public static void main(String[] args) {
 
@@ -13,7 +16,13 @@ public class JavaQuestions16 {
 // 98. Write a program that replaces all occurrences of the word "error" with "warning" in a log message.
         replaceWord("Error is error");
 
+
 // 99. Write a Java program to calculate the number of days between two dates.
+        LocalDate localDate1 = LocalDate.of(2024,7,21);
+        LocalDate localDate2 = LocalDate.of(2024,5,1);
+        System.out.println(numberOfDays(localDate1, localDate2));
+
+
 // 100. Create a method that prints the name of the day (like Monday, Tuesday, etc.) for a given date.
 // 101. Write a program that adds 30 days to the current date and prints the resulting date.
 
@@ -22,5 +31,9 @@ public class JavaQuestions16 {
     private static void replaceWord(String str){
         str = str.toLowerCase().replaceAll("error", "warning");
         System.out.println(str);
+    }
+
+    private static long numberOfDays(LocalDate date1, LocalDate date2){
+        return ChronoUnit.DAYS.between(date1, date2);
     }
 }

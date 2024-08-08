a9cf5c931dd75f3b2c57d4aa471035144244516d
Linda Busra Berg <linda.busra.berg@gmail.com>
2024-08-08 09:53:33 +0200

    last

--- src/main/java/questions/JavaQuestions18.java
+++ src/main/java/questions/JavaQuestions18.java
@@ -3,5 +3,12 @@ package questions;
 public class JavaQuestions18 {
     public static void main(String[] args) {
 
+
+// 114. Initialize a date for New Year's Day of the current year. Calculate how many days have passed since then.
+// 115. Write a Java program that determines if today's date is in a leap year.
+// 116. Develop a program that converts a string date "MM/DD/YYYY" to "YYYYMMDD".
+// 117. Write a method to print the next 10 leap years from the current year.
+// 118. Create a Java program that calculates the number of Fridays the 13th in a given year.
+
     }
 }
